c37d298d6eb3a9923fc3f4c907e34c141516fedc
Kneelawk <jedidiah.pommert@gmail.com>
2019-08-27 19:15:29 -0400

    Add simple curl functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate curl;
+
+use std::io::{stdout, Write};
+use curl::easy::Easy;
+use std::time::Duration;
+
+const URL: &str = "https://www.rust-lang.org/";
+
 fn main() {
-    println!("Hello, world!");
+    let mut easy = Easy::new();
+    easy.url(URL).unwrap();
+    easy.write_function(|data| {
+        Ok(stdout().write(data).unwrap())
+    }).unwrap();
+    easy.timeout(Duration::new(8, 0)).unwrap();
+    easy.perform().unwrap();
 }
